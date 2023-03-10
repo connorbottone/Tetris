8071a607aea93b407146072ad9fd8f0f8da098fa
connorbottone <42557300+connorbottone@users.noreply.github.com>
2023-03-10 11:49:24 -0500

    set up basis for each component and rending thru app by tetris

---
+++ tetris/src/components/Cell.js
@@ -0,0 +1,6 @@
+import React from "react";
+function Cell({ type }) {
+  return (
+  <div className={`cell ${type}`}>cell</div>
+)}
+export default Cell;
---
+++ tetris/src/components/Display.js
@@ -0,0 +1,7 @@
+import React from "react";
+function Display({ gameOver, text }) {
+  return (
+    <div >{text}display</div>
+  );
+}
+export default Display;
