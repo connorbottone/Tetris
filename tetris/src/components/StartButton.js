e70e317d8e5c4a780964cc7408e5e595b50f81cc
connorbottone <42557300+connorbottone@users.noreply.github.com>
2023-03-11 17:21:47 -0500

    finished all styling for bases of game

--- tetris/src/components/StartButton.js
+++ tetris/src/components/StartButton.js
@@ -1,8 +1,9 @@
 import React from "react";
+import { StyledStartButton } from "./styles/StyledStartButton";
 
 function StartButton({ callback }) {
     return (
-    <div>Start Game</div>
+    <StyledStartButton>Start Game</StyledStartButton>
 
     )
 }
